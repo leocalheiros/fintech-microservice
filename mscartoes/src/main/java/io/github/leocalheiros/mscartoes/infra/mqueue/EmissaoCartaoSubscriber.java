b84c4da3ebb2599c1af273d465e695fd30ca27c7
Leonardo <leonardo.calheiros@gmail.com>
2024-01-11 15:50:16 -0300

    feat: add Actuator logs and Swagger UI

--- mscartoes/src/main/java/io/github/leocalheiros/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/io/github/leocalheiros/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import io.github.leocalheiros.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
 import io.github.leocalheiros.mscartoes.infra.repository.CartaoRepository;
 import io.github.leocalheiros.mscartoes.infra.repository.ClienteCartaoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository cartaoRepository;
@@ -33,7 +35,7 @@ public class EmissaoCartaoSubscriber {
             clienteCartaoRepository.save(clienteCartao);
 
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissão de cartão: {}", e.getMessage());
         }
     }
 }
--- msclientes/src/main/java/io/github/leocalheiros/msclientes/application/ClienteService.java
+++ msclientes/src/main/java/io/github/leocalheiros/msclientes/application/ClienteService.java
@@ -3,6 +3,8 @@ package io.github.leocalheiros.msclientes.application;
 import io.github.leocalheiros.msclientes.domain.Cliente;
 import io.github.leocalheiros.msclientes.infra.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
+    private static final Logger log = LoggerFactory.getLogger(ClientesController.class);
 
     private final ClienteRepository clienteRepository;
 
